23d9d3d1d8eb775bdbe15885044ded7a7b054c92
Nasywanau25 <nasywanauraasila@gmail.com>
2024-10-17 19:32:55 +0700

    Create Tugas1.java

---
+++ Tugas1.java
@@ -0,0 +1,12 @@
+package Pertemuan_4;
+
+public class Tugas1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 6; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
